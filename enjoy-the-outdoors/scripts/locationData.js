3343ecf55a7c5f4bf65bf3dd67dd386dab337f0b
Lester Zal <lzalevskiy@appdev.yearup.org>
2023-05-23 15:11:57 -0400

    start of np page

--- enjoy-the-outdoors/scripts/locationData.js
+++ enjoy-the-outdoors/scripts/locationData.js
@@ -54,4 +54,60 @@ const locationsArray = [
     "West Virginia",
     "Wisconsin",
     "Wyoming"
-]
+];
+
+
+const selectElement = document.querySelector("#state-selection")
+
+window.onload = main
+
+function main () {
+    // loadImages()
+    populateAlbumDropdown()
+}
+
+function populateAlbumDropdown() {
+
+    // The target of a form's "submit" event is the form.
+    // The target of a click event is the element clicked.
+    // The target of a change event is the element changed. 
+
+    selectElement.onchange = handleAlbumChange
+
+    for (const album of albums) {
+        // Example: <option value="album.folder">album.name</option>
+
+        // 1. Create a new empty element by tag name:
+        const option = document.createElement("option")
+
+        // 2. Customize the new baby element:
+        option.value = album.folder
+        option.innerText = album.name
+
+        // 3. Add it to a branch which is already on the page:
+        selectElement.append(option)
+    }
+}
+
+
+
+
+
+/*
+function loadStatesDropdownList(locationArrayStateSelections) {
+  let stateNames = "";
+  for (i = 0; i < locationArrayStateSelections.length; i += 1) {
+    const stateString = locationArrayStateSelections[i]
+    stateNames += `<option value=${stateString}>${stateString}</option>`;
+  }
+  return stateNames;
+}
+   
+function addStatesToDropdownList() {
+  const selectStateElement = document. querySelector("#state-selection");
+
+  selectStateElement.innerHTML=loadStatesDropdownList(locationsArray);
+
+  selectStateElement.onchange = stateChange;
+}
+*/
