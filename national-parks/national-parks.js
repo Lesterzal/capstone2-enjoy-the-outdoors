7e728e5eee5168e357fe1f20a695ebd9ef458ba5
Lester Zal <lzalevskiy@appdev.yearup.org>
2023-05-24 15:45:50 -0400

    event listeners for elements

--- national-parks/national-parks.js
+++ national-parks/national-parks.js
@@ -1,3 +1,18 @@
 window.onload = function () {
+  const searchFilter = document.querySelector("#searchFilter");
+  searchFilter.addEventListener("change", SearchFilterDropdowns);
 
-}
+  const searchByLocation = document.querySelector("#searchByLocation");
+  searchByLocation.addEventListener("change", searchByLocationOnChange);
+
+  const searchByParkType = document.querySelector("#searchByParkType");
+  searchByParkType.addEventListener("change", searchByParkOnChange);
+
+  const searchByNationalPark = document.querySelector("#searchByNationalPark");
+  searchByNationalPark.addEventListener("change", displaysearchByNationalParkChange);
+
+  document.querySelector("#searchByLocation").style.display = "none";
+  document.querySelector("#searchByParkType").style.display = "none";
+  document.querySelector("#searchByNationalPark").style.display = "none";
+  document.querySelector("#parksDescription").style.display = "none";
+};
